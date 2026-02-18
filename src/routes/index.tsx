8375b82b1d8e6243801011384da332b8403b1086
Akr1s <dimakr71@gmail.com>
2026-02-18 20:57:17 +0200

    setup routes structure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,23 @@
 import { createBrowserRouter } from 'react-router';
 
 import App from '../App';
+import { RoutesEnum } from './routes.enum';
+import Dashboard from '../views/dashboard';
+import Onboarding from '../views/onboarding';
 
 export const router = createBrowserRouter([
     {
         path: '/',
         Component: App,
+        children: [
+            {
+                path: RoutesEnum.DASHBOARD,
+                Component: Dashboard,
+            },
+            {
+                path: RoutesEnum.ONBOARDING,
+                Component: Onboarding,
+            },
+        ],
     },
 ]);
